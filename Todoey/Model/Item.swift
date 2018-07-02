4b5683f2d15d68b2e47a456cb315a024dddd580f
Zackarin Necesito <zacknecesito@zackarins-mbp.attlocal.net>
2018-07-02 18:35:42 -0500

    convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Zackarin Necesito on 7/2/18.
+//  Copyright © 2018 Zackarin Necesito. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
